1459e3ef175a586daa09241c0db1a02783b1346f
cdvivian <cdviviany@foxmail.com>
2022-01-26 17:41:42 +0800

    Springboot基础部分学习

---
+++ springboot_01_01_quick/src/main/java/com/cd/Springboot0101QuickApplication.java
@@ -0,0 +1,13 @@
+package com.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0101QuickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot0101QuickApplication.class, args);
+	}
+
+}
