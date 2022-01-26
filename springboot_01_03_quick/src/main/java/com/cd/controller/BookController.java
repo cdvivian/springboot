e56419b9be66646183cae30e3ce4732fbba77c6f
cdvivian <cdviviany@foxmail.com>
2022-01-26 17:59:41 +0800

    Springboot基础部分学习3

---
+++ springboot_01_03_quick/src/main/java/com/cd/controller/BookController.java
@@ -0,0 +1,20 @@
+package com.cd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cdviviany
+ * @version 1.00
+ */
+//Rest模式
+ @RestController //responsebody+controller = restcontroller
+ @RequestMapping("/books")
+public class BookController {
+     @GetMapping
+     public String getById(){
+         System.out.println("springboot is running3333333333");
+         return "Springboot is running333333333";
+     }
+}
